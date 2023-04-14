23ab1dc8c1ec1c1ac38afd375fa01906c22e9f1f
Siebrex <98038100+Siebrex@users.noreply.github.com>
2023-04-14 09:15:17 -0300

    commit
    commit

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date }></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2> 
         <div className="expense-item__price" >R${props.amount}</div>
